c9c9e514145c8de5e08de69f6ff546f5b21369cb
DonCake <71788652+DonCake@users.noreply.github.com>
2023-10-10 11:26:15 -0600

    build(ios): Base proyecto

---
+++ MovieResume/MovieResume/MovieResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieResumeApp.swift
+//  MovieResume
+//
+//  Created by Diego Iturbe on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
